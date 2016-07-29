69dada7ade51bad2f0c517bde20f8836954fa51d
remicres <remi.cresson@teledetection.fr>
2016-07-29 14:11:00 +0200

    ADD: mpi_applications to ease compilation

---
+++ Modules/Remote/otb-mpi_applications/otb-module.cmake
@@ -0,0 +1,27 @@
+set(DOCUMENTATION "A bunch of otb applications in their MPI flavor")
+
+otb_module(OTBMPIApplications 
+  DEPENDS
+    OTBMPIApplicationEngine
+    OTBAppChangeDetection
+    OTBAppTextures
+    OTBAppProjection
+    OTBAppFusion
+    OTBAppClassification
+    OTBAppEdge
+    OTBAppFiltering
+    OTBAppMathParser
+    OTBAppMathParserX
+    OTBAppMoments
+    OTBAppMorphology
+	OTBAppSegmentation
+	OTBAppImageUtils
+	OTBAppIndices
+
+  TEST_DEPENDS
+    OTBTestKernel
+    OTBCommandLine
+  
+  DESCRIPTION
+    "${DOCUMENTATION}"
+  )
